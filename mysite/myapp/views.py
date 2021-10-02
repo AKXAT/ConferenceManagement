e41f516870349769951ff28edb40f896044e1b0c
AKXAT <akshat.aksharma@gmail.com>
2021-10-02 22:16:01 +0530

    Able to access Confrences from the DB to the HTML

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django import forms
+#from myapp.forms import ConferenceForm,TalkForm,SpeakerForm,ParticipantForm
+from myapp.models import ConferenceModel,TalkModel,SpeakerModal,ParticipantModel
 # Create your views here.
 
-
+def conferenceView(request):
+    conference = ConferenceModel.objects.all()
+    return render(request,'myapp/conference.html',{'conference':conference})
