969f0c06a93ea51e4b97579590adc70bd01fba5b
AKXAT <akshat.aksharma@gmail.com>
2021-10-02 15:31:03 +0530

    Made the migrations and created the superuser

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from myapp.models import ConferenceModel,TalkModel,SpeakerModal,ParticipantModel
 
+admin.site.register(ConferenceModel)
+admin.site.register(TalkModel)
+admin.site.register(SpeakerModal)
+admin.site.register(ParticipantModel)
 # Register your models here.
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 from django import forms
 # Create your views here.
+
+
