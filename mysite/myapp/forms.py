ecc3db71fbecdd4ee6a9654655c1d3dc89bb3274
AKXAT <akshat.aksharma@gmail.com>
2021-10-02 23:24:36 +0530

    Added fearture for adding a conference on the hompage

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -10,9 +10,9 @@ class ConferenceForm(forms.ModelForm):
         fields = ['conference_title','conference_description','conference_start_date','conference_end_date']
         widgets = {
             'conference_title' : forms.TextInput(attrs={'class':'form-control '}),
-            'conference_description' : forms.TextInput(attrs={'class':'form-control '}),
-            'conference_start_date' : forms.DateField(attrs={'class':'form-control','placeholder':'YYYY-MM-DD'}),
-            'conference_end_date' : forms.DateField(attrs={'class':'form-control','placeholder':'YYYY-MM-DD'})
+            'conference_description' : forms.TextInput(attrs={'class':'form-control'}),
+            'conference_start_date' : forms.TextInput(attrs={'class':'form-control','placeholder':'YYYY-MM-DD'}),
+            'conference_end_date' : forms.TextInput(attrs={'class':'form-control','placeholder':'YYYY-MM-DD'})
         }
 
 class TalkForm(forms.ModelForm):
@@ -23,8 +23,8 @@ class TalkForm(forms.ModelForm):
             'talk_conference_title' : forms.HiddenInput,
             'talk_title' : forms.TextInput(attrs={'class':'form-control '}),
             'talk_description' : forms.TextInput(attrs={'class':'form-control '}),
-            'talk_duration' : forms.DecimalField(attrs={'class':'form-control '}),
-            'talk_time' : forms.DateTimeField(attrs={'class':'form-control '}),
+            'talk_duration' : forms.TextInput(attrs={'class':'form-control '}),
+            'talk_time' : forms.TextInput(attrs={'class':'form-control '}),
         }
 
 class SpeakerForm(forms.ModelForm):
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django import forms
-#from myapp.forms import ConferenceForm,TalkForm,SpeakerForm,ParticipantForm
+from myapp.forms import ConferenceForm,TalkForm,SpeakerForm,ParticipantForm
 from myapp.models import ConferenceModel,TalkModel,SpeakerModal,ParticipantModel
 # Create your views here.
 
 def conferenceView(request):
+    if request.method == 'POST':
+        conferenceform = ConferenceForm(request.POST)
+        if conferenceform.is_valid():
+            conferenceform.save()
+            conferenceform = ConferenceForm()  
+    else:
+        conferenceform = ConferenceForm()
     conference = ConferenceModel.objects.all()
-    return render(request,'myapp/conference.html',{'conference':conference})
+    return render(request,'myapp/conference.html',{'conference':conference,'conferenceform':conferenceform})
